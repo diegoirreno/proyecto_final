21cf7b4ddf7b6c995e5843f694d2ab5166f9d2fb
diegoirreno <dfirreno@uniboyaca.edu.co>
2024-05-17 17:33:07 -0500

    interconexion interfaces y conexion base de datos

---
+++ controlador/controlador_registrousuario.php
@@ -0,0 +1,23 @@
+<?php
+if(!empty($_POST["registro"])){
+    if(empty($_POST["id"]) or empty($_POST["name"]) or empty($_POST["lastname"]) or empty($_POST["email"]) or empty($_POST["cellphone"])){
+        echo 'Uno de los campos esta vacio';
+        }else{
+        $cedula=$_POST["id"];
+        $nombre=$_POST["name"];
+        $apellido=$_POST["lastname"];
+        $email=$_POST["email"];
+        $telefono=$_POST["cellphone"];
+        $sql=$conexion->query(" insert into persona(cedula,nombres,apellidos,correo,telefono)values(' $cedula','$nombre','$apellido',' $email','$telefono')");
+        if($sql==1)
+        {
+            echo '<div>Usuario registrado correctamente</div>';
+        }else
+        {
+            echo '<div>Error al registrar el usuario</div>';
+        }
+    }
+}
+
+
+?>
---
+++ modelo/conexion_db.php
@@ -0,0 +1,7 @@
+
+<?php
+
+$conexion=new mysqli("localhost","root","","pry_final","3306");
+$conexion->set_charset("utf8");
+
+?>
