5eea3bd1784e4e53313eb948e673aeb7de2289fd
Luis Alejandro Castillo Campos <124442942+Hozu08@users.noreply.github.com>
2024-05-25 16:09:55 -0500

    Frontend indexF.php
    Se modifica el carrusel e imagenes del campo <main>, ademas se agrega el boton para dirigirse al indexAdmin.php.

--- indexF.php
+++ indexF.php
@@ -37,7 +37,7 @@
                                 </button>
                             </form>
                         </div>
-                        <div class="col-4 col-sm-6 p-0">
+                        <div class="col-2 col-sm-3 p-0">
                             <li class="nav-item d-flex align-items-center"> 
                                 <a class="nav-link" href="paginas/registrar_cli.php">Registrarse</a>
                                 <a type="button" href="paginas/colaPedido.php" class="btn btn-outline-primary">
@@ -48,7 +48,14 @@
                                 </a>
                             </li> 
                         </div>
-
+                        <div class="col-2 col-sm-3 p-0 d-flex justify-content-end">
+                            <a href="indexAdmin.php" type="button" class="btn btn-outline-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
+                                <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
+                                </svg>
+                                Administrador
+                            </a>
+                        </div>
                     </ul>
                 </div>
             </nav>
@@ -65,33 +72,27 @@
                 </div>
             </div>
             <div class="row d-flex align-items-center justify-content-center">
-                <div class="col p-0 bg-light">
+                <div class="col-4 p-0 bg-light">
                     <!--Carousel-->
                     <div id="carouselC" class="carousel slide">
                         <ol class="carousel-indicators">
-                          <li type="button" data-bs-target="#carouselC" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" ></li>
+                          <li type="button" data-bs-target="#carouselC" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></li>
                           <li type="button" data-bs-target="#carouselC" data-bs-slide-to="1" aria-label="Slide 2"></li>
-                          <!--<li type="button" data-bs-target="#carouselC" data-bs-slide-to="2" aria-label="Slide 3"></li>-->
                         </ol>
                         <div class="carousel-inner">
                           <div class="carousel-item active">
                             <a href="paginas/catalogo_producto.php">
-                                <img src="./sources/LOGO_NOVAVENTA.jpg" width="1920" height="400" class="d-block w-100" alt="slide1">
+                                <img src="./sources/Catal1.jpg" class="d-block w-100 img-fluid" alt="slide1">
                             </a>
                           </div>
                           <div class="carousel-item">
-                            <a href="paginas/catalogo_tupper.php">
-                                <img src="./sources/logo_tupper.jpg" width="1920" height="400" class="d-block w-100" alt="slide2">
+                            <a href="paginas/productos.php">
+                                <img src="./sources/Catal2.jpg" class="d-block w-100 img-fluid" alt="slide2">
                             </a>
-                          <!--</div>-->
-                          <!--<div class="carousel-item">-->
-                            <!--<a href="paginas/productos.php">-->
-                                <!--<img src="./sources/river-6858013_1920.jpg" width="1920" height="400" class="d-block w-100" alt="slide3">-->
-                            <!--</a>-->
-                          <!--</div>-->
+                          </div>
                         </div>
                         <a class="carousel-control-prev" type="button" href="#carouselC" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                           <span class="sr-only">Anterior</span>
                         </a>
                         <a class="carousel-control-next" type="button" href="#carouselC" data-bs-slide="next">
@@ -118,9 +119,9 @@
                         </p>
                     </li>
                 </ul>
-                <ul class="col mt-3 list-unstyled d-flex align-items-center">
+                <ul class="col mt-3 list-unstyled d-flex align-items-center justify-content-center">
                     <li>
-                        <img src="./sources/istockphoto-1348975688-2048x2048.jpg" class="mx-auto d-block rounded img-fluid" width="500px" alt="ejemploI">
+                        <img src="./sources/imgQ.png" class="mx-auto d-block rounded img-fluid" width="500px" alt="ejemploI">
                     </li>
                 </ul>
             </div>
