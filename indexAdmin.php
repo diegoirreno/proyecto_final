a2a0c64beb62a6cfd6f633e1414e75a9365b7057
Luis Alejandro Castillo Campos <124442942+Hozu08@users.noreply.github.com>
2024-06-09 18:35:03 -0500

    Se modifica el botón volver a menú principal.

--- indexAdmin.php
+++ indexAdmin.php
@@ -12,7 +12,7 @@
      <link rel="stylesheet" href="css/estilos.css">
      <title>Login Administrador</title>
 </head>
-<body class="container-fluid">
+<body>
     <header>
         <div class="container-fluid p-0">
             <nav class="row navbar navbar-expand-md navbar-light bg-light border-bottom border-primary">
@@ -22,14 +22,21 @@
             </nav>
             <div class="row">
                 <div class="col text-center my-3">
-                    <h5>Menú Administrador</h5>
+                    <h5>Login Administrador</h5> 
                 </div>
             </div> 
+            <div class="row">
+                <div class="col text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
+                        <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
+                    </svg>
+                </div>
+            </div>
         </div>
     </header>
-    <main class="m-0">
-    <a class="nav-link" href="../proyecto_final/indexF.php">Volver al menu principal</a>
-            <div class="contenedor__todo">
+    <main class="container-fluid mt-3">
+        <div class="row contenedor__todo">
+            <div class="col">
                 <div class="caja__trasera">
                     <div class="caja__trasera-login">
                         <h3>¿Ya tienes una cuenta?</h3>
@@ -62,10 +69,16 @@
                         <input type="password" placeholder="Contraseña" name="password_admin" required>
                         <input type="number" placeholder="Código" name="code_admin" required>
                         <button>Regístrarse</button>
-                    </form>
+                    </form> 
                 </div>
             </div>
-        </main>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a class="nav-link" href="indexF.php">Volver al menu principal</a>
+            </div>
+        </div>
+    </main>
     <footer>
         <br>
         <br>
